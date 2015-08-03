5227d53592f13e15250102571ef81198ca4b1804
Christian <chrigi@users.noreply.github.com>
2015-08-03 11:14:40 +0200

    Remove arg0 from isDeviceRooted
    arg0 is never used in the native code, undocumented and breaks the documented usage.

--- www/rootdetection.js
+++ www/rootdetection.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.isDeviceRooted = function(arg0, success, error) {
-    exec(success, error, "RootDetection", "isDeviceRooted", [arg0]);
+exports.isDeviceRooted = function(success, error) {
+    exec(success, error, "RootDetection", "isDeviceRooted", []);
 };
